d0097ad45e712b7ff5e9f068721f53cc3bb9f038
Rajinder Singh <think360@think360s-mac-mini-2.local>
2017-04-13 13:00:20 +0530

    Initial Commit

---
+++ Indus/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Indus
+//
+//  Created by think360 on 13/04/17.
+//  Copyright © 2017 bharat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Indus/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Indus
+//
+//  Created by think360 on 13/04/17.
+//  Copyright © 2017 bharat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
